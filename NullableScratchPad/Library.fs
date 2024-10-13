66665fcc8ab7ef0d23e0c46c8a2058ba7a68a777
Tomas Grosup <tomasgrosup@microsoft.com>
2024-10-13 09:36:19 +0200

    Add project files.

---
+++ NullableScratchPad/Library.fs
@@ -0,0 +1,43 @@
+﻿namespace NullableScratchPad
+
+open System.IO
+open System
+
+module ConsumeBCL =
+    let fparent f = Directory.GetParent f    
+    let fCreate f = File.Create f
+    let fExists f = File.Exists f
+
+module FSharpSyntax = 
+    type AB = A | B
+    type AbNull = AB | null
+
+    type RecordField = {X:string | null}
+    type TupleField = string * string | null
+    //type DUField = N of string | null
+
+    type DuField =  I of int | S of (string|null)
+    let showMatching (x:obj | null) =
+        match x with
+        | :? string | null -> 16
+        | _ -> 32
+
+module TypeAliasesZoo = 
+    type NullString = string | null
+    type Maybe<'a> = 'a | null
+    type 'a ``?`` = 'a | null
+    type 'a ``¯|_(ツ)_|¯`` = 'a | null
+
+    let myFunction (x:string ``?``) = x
+    let myFunction2 (x:string ``¯|_(ツ)_|¯``) = x
+
+module NewWarnings =
+    open FSharpSyntax
+    let processAB (x:AB | null) = x.IsA
+
+    let createFile (s:string|null) = ConsumeBCL.fCreate s
+
+module NullHandling = 
+    open FSharpSyntax
+    let processPerson
+
---
+++ scratchpad/Program.fs
@@ -0,0 +1,66 @@
+﻿module NullableDISABLE
+open System
+open System.IO
+
+module NullNotPossibleAtFsharpTypes = 
+
+    type Person = { Name : string}
+    type Vehicle = Car | Bike
+    type TwoStrings = string*string
+
+    let x : Person = null
+    let x : Vehicle = null
+    let x : TwoStrings = null
+    let x : {|X:int|} = null
+
+
+
+
+
+
+
+module AllowNullLiteral = 
+
+    [<AllowNullLiteral>]
+    type NullPersonRecord = { Name : string}
+
+    [<AllowNullLiteral>]
+    type NullPersonType(name:string) =
+        member _.Name = name
+
+    let x : NullPersonType = null
+
+
+
+
+
+module UncheckedOps = 
+    open NullNotPossibleAtFsharpTypes
+
+    let x  = Unchecked.defaultof<TwoStrings>
+    let manyX = Array.zeroCreate<Person> 50
+
+
+
+
+module NonFSharpTypes = 
+    let x : string = null
+    let workWithString (s:string) =
+        match s with
+        | null -> 42
+        | nonNull -> 
+            if isNull nonNull then
+                0
+            else 2112
+
+
+module GenericCode =
+    let dowork1 x =
+        let mutable z = x
+        z <- null
+        (x,z)
+
+    let dowork2 x = 
+        match x with
+        | null -> 0
+        | _ -> 1
